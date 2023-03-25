a91f765728f6c3623a492007b70c6788d56c10cd
Lindsay Powell <lindsayerin3393@gmail.com>
2023-03-25 12:13:25 -0400

    backend routes added

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,16 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const Planet = require("./models/Planet");
+const Moon = require("./models/Moon");
 
 //associations could go here!
 
 module.exports = {
   db,
   models: {
-    User,
+    Planet,
+    Moon,
   },
-}
+};
