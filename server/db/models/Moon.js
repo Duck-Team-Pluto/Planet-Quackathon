0c30434098b449f152f1bc569025db06b2bb3780
Rachel Eckert <rachel.eckert13@gmail.com>
2023-03-25 11:47:24 -0400

    added moon and planet models

---
+++ server/db/models/Moon.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Moon = db.define("moon", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  history: {
+    type: Sequelize.TEXT,
+  },
+  radiusInMiles: {
+    type: Sequelize.INTEGER,
+  },
+});
+
+module.exports = Moon;
