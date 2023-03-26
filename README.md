0107a865f9f878896feabed70f05d973cb070d28
Rachel Eckert <rachel.eckert13@gmail.com>
2023-03-26 11:47:01 -0400

    added all of the numbers to the planets

--- README.md
+++ README.md
@@ -27,3 +27,7 @@ Sync and seed your database by running `npm run seed`. Running `npm run start:de
 - start:dev will both start your server and build your client side files using webpack
 - start:dev:logger is the same as start:dev, but you will see your SQL queries (can be helpful for debugging)
 - start:dev:seed will start your server and also seed your database (this is useful when you are making schema changes and you don't want to run your seed script separately)
+
+Sources:
+https://solarsystem.nasa.gov/
+https://www.qrg.northwestern.edu/projects/vss/docs/space-environment/3-orbital-lengths-distances.html#:~:text=OBJECT,Sun
--- script/seed.js
+++ script/seed.js
@@ -6,7 +6,10 @@ const planets = [
   {
     name: "Mercury",
     radiusInMiles: 1516,
-    distanceInMiles: 29586251,
+    distanceInMiles: 3.5983125 * 10 ** 7,
+    mass: 3.285 * 10 ** 23,
+    orbitCirc: 2.23 * 10 ** 8,
+    orbitEcc: 0.20563593,
     factOne: "It is the closest planet to the sun.",
     factTwo: "It is the smallest of the non dwarf planets in our solar system.",
     factThree:
@@ -16,8 +19,10 @@ const planets = [
   {
     name: "Venus",
     radiusInMiles: 3760,
-    distanceInMiles: 66875000000,
-    // mass: 4870000000000000000000000,
+    distanceInMiles: 6.7238251 * 10 ** 7,
+    mass: 4.867 * 10 ** 24,
+    orbitCirc: 6.799 * 10 ** 8,
+    orbitEcc: 0.00677672,
     factOne:
       "It is sometimes called Earth's sister or twin planet as it is almost as large and has a similar composition.",
     factTwo:
@@ -29,7 +34,10 @@ const planets = [
   {
     name: "Earth",
     radiusInMiles: 3958.8,
-    distanceInMiles: 93000000,
+    distanceInMiles: 9.295605 * 10 ** 7,
+    mass: 5.972 * 10 ** 24,
+    orbitCirc: 5.84058059 * 10 ** 8,
+    orbitEcc: 0.01671123,
     factOne: "The Earth isn't flat",
     factTwo: "Earth rotates at around 1000 miles an hour",
     factThree:
@@ -39,7 +47,10 @@ const planets = [
   {
     name: "Mars",
     radiusInMiles: 2106.1,
-    distanceInMiles: 142000000,
+    distanceInMiles: 1.41637725 * 10 ** 8,
+    mass: 6.39 * 10 ** 23,
+    orbitCirc: 8.88 * 10 ** 8,
+    orbitEcc: 0.0933941,
     factOne: "Mars is named after the Roman god of war.",
     factTwo: "A day on Mars lasts 24 hours and 37 minutes.",
     factThree:
@@ -49,7 +60,10 @@ const planets = [
   {
     name: "Jupiter",
     radiusInMiles: 43441,
-    distanceInMiles: 484000000,
+    distanceInMiles: 4.83638564 * 10 ** 8,
+    mass: 1.89813 * 10 ** 27,
+    orbitCirc: 3.037 * 10 ** 9,
+    orbitEcc: 0.04838624,
     factOne: "Jupiter is the biggest planet in our solar system.",
     factTwo:
       "It's similar to a star, but it never got big enough to start burning.",
@@ -60,7 +74,10 @@ const planets = [
   {
     name: "Saturn",
     radiusInMiles: 36184,
-    distanceInMiles: 889000000,
+    distanceInMiles: 8.865 * 10 ** 8,
+    mass: 5.683 * 10 ** 26,
+    orbitCirc: 5.5659 * 10 ** 9,
+    orbitEcc: 0.05386179,
     factOne: "You cannot stand on Saturn.",
     factTwo:
       "Its beautiful rings are not solid. They are made up of bits of ice, dust and rock.",
@@ -70,7 +87,10 @@ const planets = [
   {
     name: "Uranus",
     radiusInMiles: 15759,
-    distanceInMiles: 1790000000,
+    distanceInMiles: 1.7837 * 10 ** 9,
+    mass: 8.681 * 10 ** 25,
+    orbitCirc: 1.12013 * 10 ** 10,
+    orbitEcc: 0.04725744,
     factOne:
       "It was the first planet found with the aid of a telescope, Uranus was discovered in 1781 by astronomer William Herschel.",
     factTwo: "Uranus is surrounded by a set of 13 rings.",
@@ -80,7 +100,10 @@ const planets = [
   {
     name: "Neptune",
     radiusInMiles: 15299,
-    distanceInMiles: 2880000000,
+    distanceInMiles: 2.7952 * 10 ** 9,
+    mass: 1.0241 * 10 ** 26,
+    orbitCirc: 1.75623 * 10 ** 10,
+    orbitEcc: 0.00859048,
     factOne:
       "Neptune is the only planet in our solar system not visible to the naked eye.",
     factTwo:
@@ -92,7 +115,10 @@ const planets = [
   {
     name: "Pluto",
     radiusInMiles: 738.38,
-    distanceInMiles: 3670000000,
+    distanceInMiles: 3.6701 * 10 ** 9,
+    mass: 1.309 * 10 ** 22,
+    orbitCirc: 2.26987 * 10 ** 10,
+    orbitEcc: 0.2482,
     factOne:
       "Pluto was considered a planet until 2006 (and it still is to us).",
     factTwo: "A year on Pluto is 248 Earth years.",
