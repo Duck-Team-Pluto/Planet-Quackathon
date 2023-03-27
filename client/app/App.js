745bc83b125902021f7b47a09fb26322ac1430bf
Lindsay Powell <lindsayerin3393@gmail.com>
2023-03-27 14:31:36 -0400

    took Planet out of App

--- client/app/App.js
+++ client/app/App.js
@@ -1,15 +1,14 @@
-
-import React from 'react';
-import StarBackground from '../styled-components/starbackground';
-import Navbar from '../features/navbar/Navbar';
-import AppRoutes from './AppRoutes';
+import React from "react";
+import StarBackground from "../styled-components/starbackground";
+import Navbar from "../features/navbar/Navbar";
+import AppRoutes from "./AppRoutes";
 
 const App = () => {
   return (
     <div id="main">
       <Navbar />
       <AppRoutes />
-      <Planet />
+      {/* <Planet /> */}
       <StarBackground></StarBackground>
     </div>
   );
