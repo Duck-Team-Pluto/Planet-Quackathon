af384ed3c48801797be3720cda411c5ae114925c
Lindsay Powell <lindsayerin3393@gmail.com>
2023-03-27 14:26:24 -0400

    planet component added, not working

--- client/app/App.js
+++ client/app/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import Navbar from '../features/navbar/Navbar';
-import AppRoutes from './AppRoutes';
+import Navbar from "../features/navbar/Navbar";
+import AppRoutes from "./AppRoutes";
+import Planet from "../features/singlePlanet/SinglePlanet";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <AppRoutes />
+      <Planet />
     </div>
   );
 };
---
+++ client/features/singlePlanet/SinglePlanet.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { selectPlanet, fetchPlanetAsync } from "./singlePlanetSlice";
+
+const Planet = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  // console.log(id);
+  const planet = useSelector((state) => state.planet);
+  console.log(planet);
+  // const { id } = useParams();
+  useEffect(() => {
+    dispatch(fetchPlanetAsync(id));
+  }, [dispatch]);
+  return (
+    <div key={planet.id}>
+      <h1>{planet.name}</h1>
+      <h2>Radius: {planet.radiusInMiles}</h2>
+      <h2>Distance from the Sun: {planet.distanceInMiles}</h2>
+      <h2>Core Type: {planet.core}</h2>
+      <h3>Fun Facts!!!</h3>
+      <h3>{planet.factOne}</h3>
+      <h3>{planet.factTwo}</h3>
+      <h3>{planet.factTwo}</h3>
+      <div>
+        {planet.moons && planet.moons.length
+          ? planet.moons.map((moon) => {
+              return (
+                <div key={moon.id}>
+                  <p>{moon.name}</p>
+                  <p>{moon.history}</p>
+                  <p>{moon.radiusInMiles}</p>
+                </div>
+              );
+            })
+          : "This Planet doesn't have any moons!"}
+      </div>
+    </div>
+  );
+};
+
+export default Planet;
