14b65ae2f10c226b14b1f7a3748683a7392648d0
maemay85 <hellomaemay@gmail.com>
2023-03-28 13:09:39 -0400

    refactored SinglePlanet to draw from separate components

---
+++ client/features/singlePlanet/Moons.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Moons = (props) => {
+
+  const moons = props.moons;
+  const planetName = props.planetName;
+
+console.log(moons)
+
+
+  return (
+    <div>
+      <h3>{planetName}'s Moons:</h3>
+      <table>
+        <thead>
+          <tr>
+            <th align="right">Name</th>
+            <th align="right">Origin of Name</th>
+            <th align="right">Radius in Miles</th>
+          </tr>
+        </thead>
+      </table>
+      {moons.map((moon) => {
+      return (
+
+          <div key={moon.id}>
+            <table>
+              <tbody>
+                <tr>
+                  <th>{moon.name}</th>
+                  <th>{moon.history}</th>
+                  <th>{moon.radiusInMiles}</th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+            );
+          })
+         }
+    </div>
+  )
+}
+
+export default Moons
--- client/features/singlePlanet/SinglePlanet.js
+++ client/features/singlePlanet/SinglePlanet.js
@@ -6,6 +6,8 @@ import {
   Rings,
   SinglePlanetImage,
 } from "../../styled-components/planetdisplays";
+import Moons from "./Moons";
+import PlanetInfo from "./PlanetInfo";
 
 const Planet = () => {
   const dispatch = useDispatch();
@@ -18,54 +20,21 @@ const Planet = () => {
   }, [dispatch]);
   return (
     <div key={planet.id}>
-      <SinglePlanetImage radius={planet.radiusInMiles}>
-        <Rings radius={planet.radiusInMiles}></Rings>
-      </SinglePlanetImage>
-      <div className="single-planet">
-        <h1>{planet.name}</h1>
-        <h2>Radius (mi): {planet.radiusInMiles}</h2>
-        <h2>Distance from the Sun (mi): {planet.distanceInMiles}</h2>
-        <h2>Core Type: {planet.core}</h2>
-        <h2>Fun Facts!!!</h2>
-        <div className="fun-facts">
-          <h3>{planet.factOne}</h3>
-          <h3>{planet.factTwo}</h3>
-          <h3>{planet.factThree}</h3>
+      <SinglePlanetImage radius={planet.radiusInMiles}></SinglePlanetImage>
+      {
+        planet && planet.name ?
+        <PlanetInfo planet={planet}></PlanetInfo>
+        : null
+      }
+      {
+        planet.moons && planet.moons.length
+        ? <Moons planetName={planet.name} moons={planet.moons}></Moons>
+        : "This planet has no moons!"
+      }
+
+      <Link to={`/planets/${next}`}>Next Planet!</Link>
+
         </div>
-        <div>
-          <h3>{planet.name}'s Moons:</h3>
-          <table>
-            <thead>
-              <tr>
-                <th align="right">Name</th>
-                <th align="right">Origin of Name</th>
-                <th align="right">Radius in Miles</th>
-              </tr>
-            </thead>
-          </table>
-          {planet.moons && planet.moons.length
-            ? planet.moons.map((moon) => {
-                return (
-                  <div key={moon.id}>
-                    <table>
-                      <tbody>
-                        <tr>
-                          <th>{moon.name}</th>
-                          <th>{moon.history}</th>
-                          <th>{moon.radiusInMiles}</th>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                );
-              })
-            : "This Planet doesn't have any moons!"}
-        </div>
-      </div>
-      <div>
-        <Link to={`/planets/${next}`}>Next Planet!</Link>
-      </div>
-    </div>
   );
 };
 
