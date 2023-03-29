422f08888f50916ebf5c5fd98db91e92cc4b6d9b
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-03-28 23:07:45 -0400

    Add in some basic info about the project and team, as well as team images

---
+++ client/features/about/aboutInfo.js
@@ -0,0 +1,31 @@
+export const aboutProject = `This is the project we did, here is information about quackathon, etc.`;
+export const aboutTeam = [
+  {
+    name: "Rachel Eckert",
+    bio: "Here is a little bio of Rachel, or maybe a few notes about her specific contributions",
+    imageLink: "images/rachel-astro.png",
+    gitLink: "https://github.com/rachel-eckert/",
+    linkedIn: "https://www.linkedin.com/in/rachel--eckert/",
+  },
+  {
+    name: "Madeleine Lloyd-Davies",
+    bio: "Here is a little bio of Madeleine, or maybe a few notes about her specific contributions",
+    imageLink: "images/madeleine-astro.png",
+    gitLink: "https://github.com/madeleine-lloyd-davies",
+    linkedIn: "https://www.linkedin.com/in/madeleine-lloyd-davies/",
+  },
+  {
+    name: "Mae May",
+    bio: "Here is a little bio of Mae, or maybe a few notes about her specific contributions",
+    imageLink: "images/mae-astro.png",
+    gitLink: "https://github.com/maemay85",
+    linkedIn: "https://www.linkedin.com/in/mae-may/",
+  },
+  {
+    name: "Lindsay Powell",
+    bio: "Here is a little bio of Lindsay, or maybe a few notes about her specific contributions",
+    imageLink: "images/lindsay-astro.png",
+    gitLink: "https://github.com/lindsayerin3393",
+    linkedIn: "https://www.linkedin.com/in/lindsay-powell3/",
+  },
+];
