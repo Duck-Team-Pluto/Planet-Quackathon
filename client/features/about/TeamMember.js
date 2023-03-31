2359eb4ecbdf253adbf78851f35af93ec6d2c4e9
Rachel Eckert <rachel.eckert13@gmail.com>
2023-03-31 19:21:49 -0400

    about page added for rachel

--- client/features/about/TeamMember.js
+++ client/features/about/TeamMember.js
@@ -4,19 +4,23 @@ const TeamMember = (props) => {
   const { name, bio, imageLink, gitLink, linkedIn } = props.person;
 
   return (
-    <div className='teamMember'>
+    <div className="teamMember">
       <div>
         <h2>{name}</h2>
         <p> {bio} </p>
         <p>
-          <a href={gitLink}>Github Profile | </a>
-          <a href={linkedIn}>LinkedIn</a>
+          <a className="about-a" href={gitLink}>
+            Github Profile |{" "}
+          </a>
+          <a className="about-a" href={linkedIn}>
+            LinkedIn
+          </a>
         </p>
         {/* note: replace with icons? */}
       </div>
       <div>
         <img
-          alt='Image of the developer wearing an astronaut helmet'
+          alt="Image of the developer wearing an astronaut helmet"
           src={imageLink}
           width={300}
           height={300}
--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -2,20 +2,23 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { NavButton } from "./NavButton";
-import { ColumnContainer, RowContainer } from "../../styled-components/Containers";
+import {
+  ColumnContainer,
+  RowContainer,
+} from "../../styled-components/Containers";
 
 const Navbar = () => {
   return (
     <div>
       <nav>
         <ColumnContainer height="12vh" padding="2vh 0 0 0">
-            <span className='navHeading'>MISSION CONTROL</span>
+          <span className="navHeading">MISSION CONTROL</span>
         </ColumnContainer>
 
-        <div className='navlinks'>
-          <Link to='/home'>Home</Link>
-          <NavButton color='#EE2E31' displayText='TEST' destination='/home' />
-          <Link to='/about'>About</Link>
+        <div className="navlinks">
+          <Link to="/home">Home</Link>
+          <NavButton color="#EE2E31" destination="/home" />
+          <Link to="/about">About</Link>
         </div>
       </nav>
     </div>
