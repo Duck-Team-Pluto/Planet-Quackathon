a1e398e7d735b61d747d4d1304b978752efcc09c
Lindsay Powell <lindsayerin3393@gmail.com>
2023-03-31 13:25:16 -0400

    homepage typewriter text added

--- client/features/home/HomeText.js
+++ client/features/home/HomeText.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { ColumnContainer } from "../../styled-components/Containers";
+import { TypewriterText } from "../../styled-components/TypewriterText";
 
 const HomeText = () => {
   return (
     <div>
-      <ColumnContainer border={true} className="single-planet-info">
+      <ColumnContainer border={false} className="single-planet-info">
         <h1>Welcome Traveller!</h1>
         <h2>You've reached the intergalactic home of W.E.L.P.</h2>
-        <h2>We 'Effing Love Planets</h2>
+        <TypewriterText>
+          <h2>We 'Effing Love Planets</h2>
+        </TypewriterText>
+        <h3>Your mission is simple: </h3>
         <h3>
-          Your mission is simple: Explore the remaining planets in this galaxy
-          and report back on where to set up our new homebase!
+          Explore the remaining planets in this galaxy and report back on where
+          to set up our new homebase!
         </h3>
         <h3>Be careful out there!</h3>
       </ColumnContainer>
