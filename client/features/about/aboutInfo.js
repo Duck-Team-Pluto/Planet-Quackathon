dda50d938547e8e714e75760c7804dd117ee6631
madeleine-lloyd-davies <mc.lloyd.davies@gmail.com>
2023-04-01 14:01:41 -0400

    add/edit bio info for madeleine and mae

--- client/features/about/aboutInfo.js
+++ client/features/about/aboutInfo.js
@@ -10,18 +10,28 @@ export const aboutTeam = [
   },
   {
     name: "Madeleine Lloyd-Davies",
-    bio: "Comics industry professional-turned-academic administrator-turned software engineer! Always excited to find opportunities to learn new things, collaborate with weirdos, do a debugging deep dive, and find ways to make someone else's life easier.",
+    bio: "Comics industry professional-turned-academic administrator-turned software engineer! Always excited to find opportunities to learn new things, collaborate with weirdos, do a debugging deep dive, and find ways to make someone else's life easier. Madeleine was responsible for wireframes and product specs, the navigation bar, and the about page.",
     imageLink: "images/madeleine-astro.png",
     gitLink: "https://github.com/madeleine-lloyd-davies",
     linkedIn: "https://www.linkedin.com/in/madeleine-lloyd-davies/",
   },
   {
     name: "Mae May",
-    bio: "Here is a little bio of Mae, or maybe a few notes about her specific contributions",
+    bio: "Mae loves to build things! Prior to becoming a software developer, she ran a woodworking business and worked in Operations in multiple industries. She is and will always be an artist at heart with extensive experience in music and dance performance and composition, as well as textile design and fabrication. For this app, Mae was the mastermind behind all of our styled components, including calculations to dynamically generate animations and size based on database info.",
     imageLink: "images/mae-astro.png",
     gitLink: "https://github.com/maemay85",
     linkedIn: "https://www.linkedin.com/in/mae-may/",
   },
+
+  Hi I'm Mae, and I love to build things!
+
+I am currently focusing on software development, having graduated in Feb 2023 from the Grace Hopper at Fullstack Academy. It is quite thrilling to plan out an application from the backend to the front, write code, debug, debug, refactor, debug, debug again, and refactor once more, and then just like *magic* to deploy and have something out in the world that can actually help people.
+
+Prior to coding, I ran a woodworking business and learned all kinds of things about working with clients, sketching out their ideas, creating a roadmap, and communicating the timeline with them, and working with a variety of components to bring the project into physical reality. It was actually this that prepared me for fullstack development - only now I'm working with code instead of, you know, wood.
+
+Additionally, I have worked in Operations in multiple industries (tech startups, a nonprofit music venue, a yarn shop) with an expansive skillset to match. I love encountering new ideas and tackling new problems. If you've worked in Operations before, you'll know that it is essentially a job of debugging in the real world!
+
+And of course, at heart, I am still and always will be an artist - and have extensive experience in music and dance performance and composition, and textile design and fabrication. Again, I love to build things :)
   {
     name: "Lindsay Powell",
     bio: "Lindsay is a former environmental scientist, currently diving into the world of software engineering! Lindsay helped with gathering backend data. She also handed several of the frontend components.",
